53ccee4d0608fb0fdc9d173c51d4e1d8068177c8
James <jamesleahy314@gmail.com>
2019-07-07 22:27:49 -0700

    inital commit

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+  ## misc 
+  alias dc="docker-compose"
+  alias d="docker"
+  alias k="kubectl"
+  alias stocks="/Users/james/software/rust/stocks/target/release/stocks"
+  alias v="nvim"
+
+  ## cargo
+  alias ct="cargo test"
+  alias cb="cargo build"
+  alias cr="cargo run"
+  alias ca="cargo add"
+  alias cs="cargo search"
+
+  ## git
+  alias gs="git status"
+  alias gp="git pull"
+  alias gc="git commit"
+  alias gm="git merge"
+  alias gf="git fetch"
+  alias gpu="git push"
+  alias gd="git diff"
+  alias g="git"
+  alias ga="git add"
+  alias gu="git update"
---
+++ nvim/init.vim
@@ -0,0 +1,10 @@
+call plug#begin('~/.local/share/nvim/plugged')
+  Plug 'rust-lang/rust.vim'
+  Plug 'dag/vim-fish'
+call plug#end()
+
+
+set tabstop=2
+set expandtab
+set shiftwidth=2
+set softtabstop=2
