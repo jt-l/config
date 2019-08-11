d2dc76f9ba1133b6625b1fcadd3c80b5eade8c57
james <jamesleahy314@gmail.com>
2019-08-11 14:30:30 -0700

    updated config

---
+++ .config/fish/config.fish
@@ -0,0 +1,66 @@
+  if status --is-login
+    exec sway
+  end
+
+  ## misc 
+  alias dc="docker-compose"
+  alias d="docker"
+  alias k="kubectl"
+  alias v="vim"
+  alias vol="alsamixer"
+  alias cls="clear"
+
+  ## cargo
+  alias ct="cargo test"
+  alias cb="cargo build"
+  alias cr="cargo run"
+  alias ca="cargo add"
+  alias cs="cargo search"
+  alias cc="cargo check"
+  alias crm="cargo rm"
+
+  ## git
+  alias gs="git status"
+  alias gp="git pull"
+  alias gc="git commit"
+  alias gm="git merge"
+  alias gf="git fetch"
+  alias gpu="git push"
+  alias gd="git diff"
+  alias g="git"
+  alias ga="git add"
+  alias gu="git update"
+
+  ## tmux 
+  alias t="tmux"
+  alias ta="tmux attach -t"
+  alias td="tmux detach"
+  alias tk="tmux kill-session -t"
+  alias tls="tmux ls"
+  alias tn="tmux new -s"
+
+  ## spotify commands
+  alias s="spotify"
+  alias sp="spotify play"
+  alias sn="clear; spotify next"
+  alias spa="spotify pause"
+  alias spre="spotify prev"
+  alias ss="spotify status"
+  alias svu="spotify vol up"
+  alias svd="spotify vol down"
+  alias sv="spotify vol"
+
+  ## spotify playlists
+  alias sp_parov="spotify play uri spotify:playlist:4n5eJ2OKEOE2lEStnUasAq"
+
+  ## weechat
+  alias chat="weechat -d ~/.config/weechat"
+
+  ## hugo
+  alias hs="hugo server"
+
+  ## rust
+  export PATH="$HOME/.cargo/bin:$PATH"
+
+  ## path to editor bin
+  export EDITOR="/usr/bin/vim"
---
+++ .vimrc
@@ -0,0 +1,10 @@
+call plug#begin('~/.vim/plugged')
+	Plug 'rust-lang/rust.vim'
+	Plug 'dag/vim-fish'
+call plug#end()
+
+set tabstop=2
+set expandtab
+set shiftwidth=2
+set softtabstop=2
+set autoindent
